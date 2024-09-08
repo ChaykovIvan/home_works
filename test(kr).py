a6ae5964c576ebccdc4cfbd18e316c548bd6ff02
Иван <ivanvhay1@mail.ru>
2024-09-08 18:29:15 +0300

    dz 08.09.24

---
+++ test(kr).py
@@ -0,0 +1,18 @@
+def drow_area():
+    for i in area:
+        print(*i)
+    print()
+
+
+area = [['*', '*', '*'], ['*', '*', '*'], ['*', '*', '*']]
+
+print('добро пожаловать в крестики-нолики')
+print('__________________________________')
+
+drow_area()
+for turn in range(1, 10)
+    print(f'ход {turn}')
+    row = int(input('Ведите номер строку')) - 1
+    column = int(input('Ведите номер калоны')) - 1
+
+    drow_area()
