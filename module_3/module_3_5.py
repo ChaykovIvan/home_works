0eaf3dc507e720b80cfcd5a47e31f968ff85a092
Иван <ivanvhay1@mail.ru>
2024-09-29 16:55:59 +0300

    redact module_3_5.py

--- module_3/module_3_5.py
+++ module_3/module_3_5.py
@@ -6,6 +6,9 @@ def get_multiplied_digits(number):
 
     first = int(str_number[0])
 
+    if first == 0:
+        return get_multiplied_digits(int(str_number[1:]))
+
     return first * get_multiplied_digits(int(str_number[1:]))
 
 
